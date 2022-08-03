2fca4060908eefd2668de8bf90f9d185932b0086
Jagadesh-Ronanki <jagadesh.ronanki@gmail.com>
2022-08-03 21:39:08 +0530

    fix-bug-ItemPreview

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,6 +37,10 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; 
+          currentTarget.src="placeholder.png";
+        }}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
